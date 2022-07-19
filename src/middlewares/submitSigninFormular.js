9cd12e8c6724f093887cc201bc354928b1ff3537
patrickletourneux <patrick.letourneux20@gmail.com>
2022-07-19 15:30:43 +0200

    axios signin and signup ok

--- src/middlewares/submitSigninFormular.js
+++ src/middlewares/submitSigninFormular.js
@@ -1,12 +1,24 @@
 
+import api from '../axiosInstance';
+
 import { changeIsUserConnectedToTrue } from '../actions'
 
 const submitSigninFormular = (store) => (next) => (action) => {
   // console.log('submitSignupFormular middleware', action);
   switch(action.type){
     case 'SUBMIT_SIGNIN_FORMULAR':
-        console.log('to send axios')
+        console.log('to send axios');
+        api.post('signin', {
+          email: action.userEmail,
+          password: action.userPassword,
+        }).then((response) => {
+        console.log('response.data',response.data)
         store.dispatch(changeIsUserConnectedToTrue(true));
+        next(action);
+
+        })
+        .catch((error) => console.log('error ',error));
+
         // next(action);
         break
     default:
